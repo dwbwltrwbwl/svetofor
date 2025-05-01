d447b407a31266672818e16b024c702539c2ac51
dwbwltrwbwl <tyumenelizaveta@yandex.ru>
2025-05-01 17:44:05 +0300

    blablabla

--- app/src/main/java/com/example/svetofor/MainActivity.kt
+++ app/src/main/java/com/example/svetofor/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.example.svetofor
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var redLight: View
+    private lateinit var yellowLight: View
+    private lateinit var greenLight: View
+
+    private var currentState = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        if (savedInstanceState != null) {
+            currentState = savedInstanceState.getInt("CURRENT_STATE", 0)
         }
+
+        redLight = findViewById(R.id.redLight)
+        yellowLight = findViewById(R.id.yellowLight)
+        greenLight = findViewById(R.id.greenLight)
+
+        val changeButton = findViewById<View>(R.id.changeButton)
+        changeButton.setOnClickListener { changeLight() }
+
+        updateLights()
+    }
+
+    private fun changeLight() {
+        currentState = (currentState + 1) % 4
+        updateLights()
+    }
+
+    private fun updateLights() {
+
+        redLight.setBackgroundResource(R.drawable.circle_gray)
+        yellowLight.setBackgroundResource(R.drawable.circle_gray)
+        greenLight.setBackgroundResource(R.drawable.circle_gray)
+
+        when (currentState) {
+            0 -> redLight.setBackgroundResource(R.drawable.circle_red)
+            1 -> yellowLight.setBackgroundResource(R.drawable.circle_yellow)
+            2 -> greenLight.setBackgroundResource(R.drawable.circle_green)
+            3 -> yellowLight.setBackgroundResource(R.drawable.circle_yellow)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("CURRENT_STATE", currentState)
     }
 }
